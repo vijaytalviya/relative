6a5b0cccb027b16d9c186c8be30197d8c64c43d4
vijay <talviyav.com>
2022-07-31 18:20:01 +0530

    icons & data

---
+++ src/assets/contant.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    currencyName: "Bitcoin(BTC)",
+    Price: 31812.8,
+    change: 10,
+    TVL: 60000,
+  },
+  {
+    currencyName: "solana(SQL)",
+    Price: 32.83,
+    change: -12.32,
+    TVL: 60000,
+  },
+  {
+    currencyName: "Ethereum(ETH)",
+    Price: 1466.45,
+    change: -11.93,
+    TVL: 60000,
+  },
+  {
+    currencyName: "Binance USD (BUSD)",
+    Price: 1.01,
+    change: 0.26,
+    TVL: 60000,
+  },
+  {
+    currencyName: "Shib Inu (SHIB)",
+    Price: 0.00000001948,
+    change: -8.1,
+    TVL: 60000,
+  },
+];
